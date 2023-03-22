f44fa3ea46b10fc90b4d067a3e7334a6e7702920
Evandro Oneda <evandro.oneda@hotmail.com>
2023-03-22 19:53:38 -0300

    exemplo for

---
+++ Problem-dio/src/Problem.java
@@ -0,0 +1,14 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class Problem {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+// TODO: complete os espaços em branco com sua solução para o problema
+		String[] nomes = new String[     ];
+		
+		for(int i=0 ; i<10 ; i++)
+			nomes[    ] = sc.nextLine();
+
+	}
+}
